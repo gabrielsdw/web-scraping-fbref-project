82766fb02f4941ebf08857a96d5d10db4984c927
gabrielsdw <gabriel2006adriano74@gmail.com>
2024-04-11 22:45:05 -0300

    Pegando time da casa e visitante, gols da casa e visitante, data e campeonato.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions, Edge, EdgeOptions, Firefox, FirefoxOptions
 from selenium.webdriver.common.by import By
 
 from time import sleep
+
+import pandas as pd
 
 import re
 
@@ -9,8 +11,10 @@ import re
 
 class WsFbref:
     def __init__(self):
-        self.driver = Chrome()
-        self.driver.implicitly_wait(0.5)
+        self.options = ChromeOptions()
+        
+        self.driver = Chrome(options=self.options)
+        #self.driver.implicitly_wait(2)
 
         self.url_base = "https://fbref.com"
         self.url_camp = f"{self.url_base}/en/comps/9/Premier-League-Stats"
@@ -56,33 +60,81 @@ class WsFbref:
         for time, link in zip(times, links_times):
             self.driver.get(link)
             
-            
             tags_partidas = self.driver.find_elements(By.XPATH, "//td[@data-stat='match_report']")
             tags_datas_partidas = self.driver.find_elements(By.XPATH, "//th[@data-stat='date' and @class='left ']")
+            tags_campeonatos_partidas = self.driver.find_elements(By.XPATH, '//td[@data-stat="camp"]')
+            tags_campeonatos_partidas = tags_campeonatos_partidas[:len(tags_partidas)]
             
             links = []
             datas = []
+            campeonatos = []
             
-            for tag_data_partida, tag_partida in zip(tags_datas_partidas, tags_partidas):
+            for tag_campeonato_partida, tag_data_partida, tag_partida in zip(tags_campeonatos_partidas, tags_datas_partidas, tags_partidas):
                 if str(tag_partida.text).lower() != 'head-to-head':
                     link_partida = tag_partida.find_element(By.TAG_NAME, 'a')
                     link_partida = link_partida.get_attribute('href')
                     
                     data_partida = str(tag_data_partida.text)
-                    
+
+                    campeonato_partida = tag_campeonato_partida.find_element(By.TAG_NAME, 'a')
+                    campeonato_partida = str(campeonato_partida.text)
+
+
+                    campeonatos.append(campeonato_partida)
                     links.append(link_partida)
                     datas.append(data_partida)
-                 
-           
-               
-             
                     
             dados[time] = {
                 'links': links,
-                'datas': datas
+                'datas': datas,
+                'campeonatos': campeonatos
             }
 
         return dados
+    
+    
+    def retorna_estatisticas_por_time(self, times, dados):
+
+        for time in times:
+            for camp, data, link in zip(dados[time]['campeonatos'][:5], dados[time]['datas'][:5], dados[time]['links'][:5]):
+                self.driver.get(link)
+
+                homeTeam, awayTeam = self.retorna_home_e_away_team()
+                
+                fthg, ftag = self.retorna_gols_home_e_away_team()
+
+                print(homeTeam, awayTeam, fthg, ftag, data, camp)
+                sleep(2.5)
+
+
+    def retorna_home_e_away_team(self):
+        teams = self.driver.find_element(By.TAG_NAME, 'h1')
+        teams = str(teams.text)
+        teams = teams.split('Match')
+        teams = teams[0]
+        teams = ''.join(teams)
+        teams = teams.split('vs.')
+        
+        homeTeam, awayTeam = teams
+        
+        return homeTeam.strip(), awayTeam.strip()
+    
+
+    def retorna_gols_home_e_away_team(self):
+        scores = self.driver.find_elements(By.XPATH, '//div[@class="score"]')
+            
+        lista = []
+
+        for score in scores:
+            lista.append(str(score.text))
+
+        fthg, ftag, *_ = lista
+
+        return fthg, ftag
+
+
+       
+                
                 
             
                     
@@ -92,7 +144,10 @@ class WsFbref:
 
 if __name__ == '__main__':
     obj = WsFbref()
+    
     info = obj.retorna_info_time()
     
-    obj.retorna_partidas_por_time(info['times'][:2], info['links'][:2])
+    dados = obj.retorna_partidas_por_time(info['times'][:2], info['links'][:2])
+
+    obj.retorna_estatisticas_por_time(info['times'][:2], dados)
    
